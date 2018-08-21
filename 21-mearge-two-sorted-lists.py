861e3f60bb9bb882af0c1f36975a45ae8e92bfe6
starrysky1211 <starry_sky_@outlook.com>
2018-08-21 21:07:26 +0800

    linked list easy first 3 problems. total 10

---
+++ 21-mearge-two-sorted-lists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        holder = ListNode(0)
+        head = holder
+        while l1 and l2:
+            if l1 and l2:
+                if l1.val < l2.val:
+                    head.next = l1
+                    l1 = l1.next
+                else:
+                    head.next = l2
+                    l2 = l2.next
+                head = head.next
+        head.next = l1 or l2
+        return holder.next
